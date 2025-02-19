683bc9cb2054855d4ce4f5f9487c3fb161f41f15
sskcalo <107052014+sskcalo@users.noreply.github.com>
2025-02-19 20:58:14 +0900

    Delete lp1 directory

--- lp1/assets/script/hamburger.js
+++
@@ -1,9 +0,0 @@
-$(document).ready(function () {
-    $("#nav-button").on("click", function () {
-        $(".sp-nav").slideToggle();
-    });
-});
-
-$('.sp-nav a[href]').on('click', function (event) {
-    $('.nav-button').trigger('click');
-});
